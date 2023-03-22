ba55fca1add72d526707396180c6fb6b20978162
Ahmad-abughaush <ahmedabughoshh@gmail.com>
2023-03-22 16:26:03 +0300

    dsad

--- app.js
+++ app.js
@@ -1,55 +1,47 @@
+const form = document.getElementById("myForm");
 
-let name = prompt("please enter your name");
-let age = prompt("please enter your age");
-let gender = "";
-let array = [];
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); // prevent the default form submission
 
-function mygender(x, y) {
-    while (gender !== x && gender !== y) {
-        gender = prompt("enter your gender");
-    }
-}
-mygender("male", "female")
+    let username = event.target.username.value;
+    let age = event.target.age.value;
+    let drink = event.target.drink.value;
+    let drinktype = event.target.drinktype.value;
 
 
-let drink = prompt("enter your drink  ");
-if ( drink=="cold" && drink == "hot") { alert("ok") }
-let nameofdrink = prompt(" write the drink`s name ");
-{ alert('your drink is getting prepared '); }
+    let dom = document.getElementById("dom")
+    let div = document.createElement("div")
 
-array.push(name, gender, age, nameofdrink)
-for (let i = 0; i < array.length; i++) { console.log(array[i]) }
+    let p = document.createElement("p")
+    p.textContent = "name: " + username
+
+
+    let ul = document.createElement("ul")
 
 
 
+    let li2 = document.createElement("li")
+    li2.textContent = "age : " + age
+    let li3 = document.createElement("li")
+    li3.textContent = "drink" + drink + " " + drinktype
+
+
+
+    dom.appendChild(div)
+    div.appendChild(p)
+    div.appendChild(ul)
+    ul.appendChild(li2)
+    ul.appendChild(li3)
 
 
 
 
-let dom = document.getElementById("dom")
-console.log(dom)
-let div = document.createElement("div")
-
-let p = document.createElement("p")
-p.textContent = "name: "+name
 
 
-let ul = document.createElement("ul")
-
-let li1 = document.createElement("li")
-li1.textContent = "gender:"+ gender 
-
-let li2 = document.createElement("li")
-li2.textContent = "age : "+ age
-let li3 = document.createElement("li")
-li3.textContent = "drink"+ drink+" "+nameofdrink
+    form.reset();
 
 
 
-dom.appendChild(div)
-div.appendChild(p)
-div.appendChild(ul)
-ul.appendChild(li1)
-ul.appendChild(li2)
-ul.appendChild(li3)
 
+
+});
